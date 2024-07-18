94dc2f257190a9a184d63273c0395f57c0d6271e
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-07-18 11:46:25 +0500

    Create dataTypes.js

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,14 @@
+let age = 18;
+let isloggedIn = false;
+
+//number => 2 to powser 53
+//bigint
+//string =""
+//boolean =>true/false
+//null standalone value
+//undefined =>
+//symbol => unique
+
+//Objects
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
