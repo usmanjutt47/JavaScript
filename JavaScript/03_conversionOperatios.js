681a1e99aab13bf817c9ec697e5e35a6c2155abe
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-07-18 13:23:50 +0500

    operations

--- JavaScript/03_conversionOperatios.js
+++ JavaScript/03_conversionOperatios.js
@@ -21,5 +21,35 @@ let booleanLoggendIn = Boolean(loggendIn);
 
 let someNumber = 447;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************** Operations ********************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello, ";
+let str2 = "Usman";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 + 1);
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
