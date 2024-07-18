e73abe0825a7a8bf0c08df737e083047fc5cd8b1
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-07-18 12:46:46 +0500

    Create 03_conversionOperatios.js

---
+++ JavaScript/03_conversionOperatios.js
@@ -0,0 +1,25 @@
+let score = false;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let loggendIn = "usman";
+let booleanLoggendIn = Boolean(loggendIn);
+// console.log(booleanLoggendIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Usman" => true
+
+let someNumber = 447;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
