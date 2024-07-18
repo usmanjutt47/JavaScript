d24bd121f4d56c5edb68c57d1a76884a78d7d727
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-07-18 11:37:07 +0500

    Create variables.js

---
+++ JavaScript/variables.js
@@ -0,0 +1,13 @@
+const accountId = 31919191
+let accountEmail = "usmanjutt04747@gmail.com"
+var accountPassword = "72418"
+var accountCity = "FSD"
+
+let accountState;
+
+accountCity = "Samundri"
+accountEmail = "samundri@gmail.com"
+accountPassword = "21212121"
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState]);
